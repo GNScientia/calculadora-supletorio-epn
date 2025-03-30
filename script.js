8278e8691e93ffa7acb25da7655d8066f929b726
GNScientia <162643735+GNScientia@users.noreply.github.com>
2025-03-30 18:53:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ function calcularNota() {
     var notaSemestre = parseFloat(document.getElementById("nota_semestre").value);
 
     if (isNaN(notaSemestre) || notaSemestre < 0 || notaSemestre > 40) {
-        document.getElementById("resultado").innerText = "Por favor ingresa una nota válida entre 0 y 40.";
+        document.getElementById("resultado").innerText = "Por favor ingresa una nota válida entre 0 y 40.📝";
         return;
     }
 
     if (notaSemestre >= 28) {
         // Si la nota del semestre es 28 o mayor
-        document.getElementById("resultado").innerText = "¡Aprobaste el semestre!";
+        document.getElementById("resultado").innerText = "🎊¡Aprobaste el semestre, Felicidades!🎉";
     } else if (notaSemestre < 18) {
         // Si la nota del semestre es menor a 18
-        document.getElementById("resultado").innerText = "No alcanzaste el supletorio y lamentablemente perdiste la asignatura.";
+        document.getElementById("resultado").innerText = "😭No alcanzaste el supletorio y lamentablemente perdiste la asignatura.😖";
     } else {
         // Si la nota es entre 18 y 27
         var notaSupletorio = Math.max(24, (24 * 2) - notaSemestre);
         document.getElementById("resultado").innerText =
-            "El estudiante necesita sacar al menos una nota de " + notaSupletorio.toFixed(2) + " en el supletorio para pasar.";
+            "El estudiante necesita sacar al menos una nota de " + notaSupletorio.toFixed(2) + " en el supletorio para pasar. ¡Mejor ponte a Estudiar!😱";
     }
 }
 
 // Función para redirigir al usuario a otra página cuando se presione el botón
 function redireccionar() {
-    window.location.href = "https://www.ejemplo.com";  // Cambia esto por la URL de la página de destino
+    window.location.href = "gnscientia.ec";  // Cambia esto por la URL de la página de destino
 }
